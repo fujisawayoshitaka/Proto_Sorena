a268cff008184b062abe689dcb4cc75db1ce8066
fujisawayoshitaka <1317699@gmail.com>
2020-02-07 15:14:45 +0900

    device実装テスト

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :blogs
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'blogs#index'
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+end
